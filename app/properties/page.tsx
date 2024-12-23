417ef69c9e6e9750a2d0c15f134ab14838f9662e
khtab <khtab@mvp-apps.ae>
2024-12-24 00:17:09 +0400

    implemented

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -50,7 +50,7 @@ export default function PropertiesPage() {
     >
       <ListingHero />
 
-      <div className="container mx-auto px-4 md:px-6 lg:px-8 xl:px-16 py-8">
+      <div className=" mx-10 md:mx-auto px-4 md:px-6 lg:px-8 xl:px-12 2xl:px-16 py-8">
         <div className="flex flex-col md:flex-row justify-between items-start gap-6">
           <ListingFilters
             filters={filters}
